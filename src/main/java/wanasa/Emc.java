b44cc5633a75edede25e483447669914be688318
pallavidomreddy <pallavi.domreddy@smscountry.com>
2019-07-23 11:35:33 +0530

    test

--- src/main/java/wanasa/Emc.java
+++ src/main/java/wanasa/Emc.java
@@ -1,11 +1,14 @@
 package wanasa;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-
-import utils.BrowserFunctions;
 import utils.CommonMethods;
 
 public class Emc extends CommonMethods {
@@ -93,6 +96,11 @@ public class Emc extends CommonMethods {
 			By incorrEmail				    =	By.xpath("//*[contains(@class,'toast-message') and contains(text(),'EmailId not Registered')]");
 			By incorrpaswd					=	By.xpath("//*[contains(@class,'toast-message') and contains(text(),'Password Incorrect')]");
 		
+			
+			//logout 
+			
+			By profiledropdown = By.id("loginDp");
+			By logout 		   = By.xpath("//a[@id='logOut']");
 		
 		public String signInEmptyEmail() 
 		{
@@ -102,7 +110,7 @@ public class Emc extends CommonMethods {
 				driver.findElement(pwd).sendKeys(CommonMethods.passingData("EmcPassword"));
 				Thread.sleep(2000);
 				driver.findElement(logingBtn).click();
-				Thread.sleep(4000);
+				Thread.sleep(2000);
 			}
 			catch(Exception ex)
 			{
@@ -156,7 +164,7 @@ public class Emc extends CommonMethods {
 				try
 				{
 					driver.findElement(usermail).clear();
-					driver.findElement(usermail).sendKeys(CommonMethods.passingData("EnterMail"));
+					driver.findElement(usermail).sendKeys(CommonMethods.passingData("emcmail"));
 					Thread.sleep(2000);
 					driver.findElement(pwd).clear();
 					driver.findElement(pwd).sendKeys(CommonMethods.passingData("EmcPassword"));
@@ -171,4 +179,340 @@ public class Emc extends CommonMethods {
 				String url=driver.getCurrentUrl();
 				return url;
 		}
+		
+		
+		//event creation 
+		
+			By eventsclick	= By.id("events");
+			By createEvent	= By.id("createEventM");
+			By eventName	= By.id("txtEventName");
+			By venue		= By.id("txtVenue");
+			By location		= By.id("txtLocation");
+			By selectloc	= By.xpath("//*[@class='pac-container pac-logo']//span[contains(text(),'rain International Airport')]");
+			By startDate	= By.id("txtStDate");
+			By dateselections = By.xpath("//*[@class='ui-state-default']");
+			By endDate		= By.id("txtEdDate");
+			By startTime	= By.id("txtStTime");
+			By endTime		= By.id("txtEdTime");
+			
+			//timeslot1
+			By addslot		= By.xpath("//*[@id='btnAddMultipleSlots']/i");
+			By starttime1   = By.id("txtStTime1");
+			By endtime1		= By.id("txtEdTime1");
+			
+			
+			// repeate event 	
+			By toggleyes 	= By.xpath("//*[@id='uniform-repeat']//span[2]	");	
+			
+			
+			// repeate event 
+			By ddlrpt		= By.id("ddlRepeat");	
+			By save			= By.id("btnSaveSchedule");
+			
+		
+			
+			By eventDescr	= By.xpath("//*[@id='event_panel']/div[2]/div[6]/div/div/div");		
+			By thumbImage	= By.id("thumbUpload");
+			By saveChanges1	= By.linkText("Save changes");
+			By next1		= By.id("btnCreateEventNext");
+			
+			//section2
+			//subcatfree
+			By freecat		= By.id("btnFreeTicket");
+			By freecatName	= By.id("txtFTicketName_1");
+			By maxTicketsfree	= By.id("freeTicketsMax_1");
+			By subcatfreeclick  = By.id("fsub_1");
+			By subcatfree1      = By.id("fname_1");
+			By subcatfree2      = By.id("fname_2");
+			By additionalfields = By.id("showFree_1");
+			By savechangesfree  = By.id("saveFTicket_1");
+			
+			//subcat paid1 
+			By paidcat		= By.id("btnPaidTicket");
+			By paidName		= By.id("paidTicketName_1");
+			By paidtktstotal= By.id("paidTicketsQuantity_1");
+			By paidsubcatadd  = By.id("pSub_1");
+			By paidsubcat1  = By.id("pName_1");
+			By paidsubcat1cost1 =By.id("pPrice_1");
+			By paidsubcat2  = By.id("pName_2");
+			By paidsubcat1cost2 =By.id("pPrice_2");
+			By paidadditional = By.id("showPaid_1");		
+			By savepaid1	= By.id("savePTicket_1");
+			
+			//subcat paid1 
+			By paidName2		= By.id("paidTicketName_2");
+			By paidtktstotal2= By.id("paidTicketsQuantity_2");
+			By paidsubcatadd2  = By.id("pSub_2");
+			By paidsubcat3  = By.id("pName_3");
+			By paidsubcat1cost3 =By.id("pPrice_3");
+			By paidsubcat4  = By.id("pName_4");
+			By paidsubcat1cost4 =By.id("pPrice_4");
+			By paidadditional2 = By.id("showPaid_2"); //maxPTickets_2
+			By savepaid2	= By.id("savePTicket_2");	
+			
+			By next2		= By.id("btnCreateTicketsSectionNext");
+			
+			By paidPrice	= By.id("ticketPrice_1");
+			By maxticketsPaid	= By.id("paidTicketsQuantity_1");
+			By saveChangesP	= By.id("savePTicket_1");
+			//By next3	= By.id("btnCreateTicketsSectionNext");
+			By orgName		= By.id("txtOrgName");
+			By orgEmail		= By.id("txtOrgEmail");
+			By orgMobile	= By.id("txtOrgMobile");
+			By orgCompany	= By.id("txtOrgCompany");
+			By orgDescri	= By.id("txtOrgAbout");
+			By next3		= By.id("btnCreateOrgSectionNext");
+			By termsAndCon	= By.xpath("//div[@class='form-group margin-bottom-30']//div[@class='redactor_form-control redactor_editor']");
+			By eventType	= By.id("ddlEventType");
+			By eventCategory	= By.id("ddlEventTopic");
+			By pageName		= By.id("friendlyUrl");
+			By langues		=	By.xpath("//div[@class='col-md-6']//button[@class='multiselect dropdown-toggle btn btn-default']");
+			By checkBox		=	By.xpath("//div[@class='btn-group open']//ul[@class='multiselect-container dropdown-menu']");
+			By checkarabic  = By.xpath("//*[@class='checkbox']//*[@value='1']");
+			By checkenglish = By.xpath("//*[@class='checkbox']//*[@value='2']");
+			By additionalaNote	= By.id("addtionalNote");
+			By publi		=	By.id("public");
+			By saveEventBtn	= By.id("btnSaveEvent");
+			By ok						=	By.id("btnOk");
+			By eventsuccess	=	By.xpath("//*[@id='eventSuccessMsg']");
+			
+			public void evntcreationmultisubcattimeslot() throws InterruptedException, AWTException
+			{
+				driver.navigate().to("http://staging.emc.wanasatime.com/Dashboard.aspx");
+				driver.findElement(eventsclick).click();
+				driver.findElement(createEvent).click();
+				driver.findElement(eventName).sendKeys("standup comedy");
+				driver.findElement(venue).sendKeys("Bahrain");
+				//Bahrain International Airport, Road 2404, Muharraq, Bahrain
+				driver.findElement(location).sendKeys("Bah");
+				driver.findElement(selectloc).click();
+				driver.findElement(startDate).click();
+				Thread.sleep(2000);
+
+				//startdate 				
+				List<WebElement> sd = driver.findElements(dateselections);
+				System.out.println("total days :"+sd.size());
+				for(WebElement date1 : sd)
+				{
+					String sdate = date1.getText();
+					if(sdate.equalsIgnoreCase("15"))
+					{
+						Thread.sleep(2000);
+						date1.click();
+						Thread.sleep(2000);
+						break;
+					
+					}
+				}
+				
+				//enddate
+				driver.findElement(endDate).click();
+				List<WebElement> ed = driver.findElements(dateselections);
+				System.out.println("total days :"+ed.size());
+				for(WebElement date1 : ed)
+				{
+					String edate = date1.getText();
+					if(edate.equalsIgnoreCase("30"))
+					{
+						Thread.sleep(2000);
+						date1.click();
+						Thread.sleep(2000);
+						break;					
+					}
+				}
+								
+				//time slots
+				//slot1	
+				driver.findElement(startTime).clear();
+				driver.findElement(startTime).sendKeys("11:00 AM");
+				driver.findElement(endTime).clear();
+				driver.findElement(endTime).sendKeys("12:30 PM");
+				Thread.sleep(2000);
+				
+				
+				 driver.findElement(addslot).click();
+				 Thread.sleep(2000);
+				 JavascriptExecutor js = (JavascriptExecutor) driver;
+				 js.executeScript("window.scrollBy(0,600)");
+				 Thread.sleep(2000);
+				//slot2
+				driver.findElement(starttime1).clear();
+				driver.findElement(starttime1).sendKeys("05:00 PM");
+				driver.findElement(endtime1).clear();
+				driver.findElement(endtime1).sendKeys("06:30 PM");
+				Thread.sleep(2000);
+						
+				// repeateevent
+				driver.findElement(toggleyes).click();
+				Thread.sleep(2000);
+			    Select ddlrept = new Select(driver.findElement(ddlrpt));
+			    ddlrept.selectByValue("Daily");
+			    Thread.sleep(2000);
+			    driver.findElement(save).click();
+			    Thread.sleep(5000);		
+			    
+			    driver.findElement(eventDescr).click();
+			    driver.findElement(eventDescr).sendKeys("hello");
+			    
+
+				 driver.findElement(thumbImage).click();		
+				 Thread.sleep(3000);
+				 StringSelection ss= new StringSelection("C:\\Users\\Smsc_Tech\\Desktop\\mus6.jpg");
+				 Thread.sleep(8000);
+				 Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss,null);
+				 Robot robot = new Robot();
+				 robot.keyPress(KeyEvent.VK_CONTROL);
+				 robot.keyPress(KeyEvent.VK_V);
+				 robot.keyRelease(KeyEvent.VK_V);
+				 robot.keyRelease(KeyEvent.VK_CONTROL);
+				 robot.keyPress(KeyEvent.VK_ENTER);
+				 robot.keyRelease(KeyEvent.VK_ENTER);
+				 Thread.sleep(14000);
+				 driver.findElement(saveChanges1).click();
+				 Thread.sleep(4000);
+				 driver.findElement(next1).click();
+				 Thread.sleep(10000);
+				 
+				 ///////section2 
+				 ////add subcat(free)
+				 
+				 driver.findElement(freecat).click();
+				 driver.findElement(freecatName).sendKeys("Free");
+				 Thread.sleep(2000);
+				 driver.findElement(maxTicketsfree).sendKeys("100");
+				 Thread.sleep(2000);
+				 //subcat1
+				 driver.findElement(subcatfreeclick).click();
+				 driver.findElement(subcatfree1).click();
+				 driver.findElement(subcatfree1).sendKeys("Audlt");
+				 Thread.sleep(2000);
+				 //subcat2 additionalfields
+				 driver.findElement(subcatfreeclick).click();
+				 driver.findElement(subcatfree2).click();
+				 driver.findElement(subcatfree2).sendKeys("senior citizen");
+				 Thread.sleep(2000);
+				 driver.findElement(additionalfields).click();
+				 Thread.sleep(2000);
+				 Select maxtkt= new Select(driver.findElement(By.id("freeTicketsMaxAllowed_1")));
+				 maxtkt.selectByValue("4");
+				 driver.findElement(savechangesfree).click();
+				 Thread.sleep(3000);
+				 
+				 ////paid cat/subcat(gold)	
+				// js.executeScript("arguments[0].scrollIntoView();", paidcat);
+				 driver.findElement(paidcat).click();
+				// js.executeScript("arguments[0].scrollIntoView();", paidName);
+				 driver.findElement(paidName).sendKeys("Gold");
+				 Thread.sleep(2000);
+				 driver.findElement(paidtktstotal).sendKeys("200");
+				
+				 
+				 //paid subcat1(gold)
+				 driver.findElement(paidsubcatadd).click();
+				 Thread.sleep(2000);				
+				 driver.findElement(paidsubcat1).click();
+				 driver.findElement(paidsubcat1).sendKeys("Audlt");
+				 driver.findElement(paidsubcat1cost1).sendKeys("5");
+				 Thread.sleep(2000);
+				 
+				 //paid subcat2 paidtktstotal(gold)
+				 driver.findElement(paidsubcatadd).click();
+				 Thread.sleep(2000);				
+				 driver.findElement(paidsubcat2).click();
+				 driver.findElement(paidsubcat2).sendKeys("Senior citizen");
+				 driver.findElement(paidsubcat1cost2).sendKeys("8");
+				 Thread.sleep(2000);
+				 js.executeScript("window.scrollBy(0,400)");
+				 Thread.sleep(2000);
+				 driver.findElement(paidadditional).click();				
+				 Thread.sleep(2000);	
+				 js.executeScript("window.scrollBy(0,1000)");
+				 Select maxtkts= new Select(driver.findElement(By.id("maxPTickets_1")));
+				 maxtkts.selectByValue("3");
+				 Thread.sleep(2000);
+				 //js.executeScript("window.scrollBy(0,600)");
+				 driver.findElement(savepaid1).click();
+				 Thread.sleep(2000);
+				 driver.findElement(next2).click();
+				 Thread.sleep(4000);
+				 
+                 /*				 
+				 //paid subcat1(silver)  
+				 driver.findElement(paidcat).click();
+				 driver.findElement(paidName2).sendKeys("Silver");
+				 Thread.sleep(2000);
+				 driver.findElement(paidtktstotal2).sendKeys("200");
+				 
+				 //paid subcat1(silver)
+				 driver.findElement(paidsubcatadd2).click();
+				 Thread.sleep(2000);				
+				 driver.findElement(paidsubcat3).click();
+				 driver.findElement(paidsubcat3).sendKeys("Audlt");
+				 driver.findElement(paidsubcat1cost3).sendKeys("10");
+				 Thread.sleep(2000);
+				 
+				 //paid subcat2 paidtktstotal(silver)
+				 driver.findElement(paidsubcatadd2).click();
+				 Thread.sleep(2000);				
+				 driver.findElement(paidsubcat4).click();
+				 driver.findElement(paidsubcat4).sendKeys("Senior citizen");
+				 driver.findElement(paidsubcat1cost4).sendKeys("8");
+				 Thread.sleep(2000);
+				 driver.findElement(paidadditional2).click();
+				 Thread.sleep(2000);
+				 Select maxtkts1= new Select(driver.findElement(By.id("maxPTickets_2")));
+				 maxtkt.selectByValue("4");
+				 driver.findElement(savepaid2).click();
+				 */
+				 
+				 
+				 //section3
+				 driver.findElement(orgName).sendKeys("Manjusha");
+				 Thread.sleep(2000);
+				 driver.findElement(orgEmail).sendKeys(CommonMethods.passingData("EnterMail"));
+				 Thread.sleep(2000);
+				 driver.findElement(orgMobile).sendKeys("9866585555");
+				 Thread.sleep(2000);
+				 driver.findElement(orgCompany).sendKeys("Event Easy");
+				 Thread.sleep(2000);
+				 driver.findElement(orgDescri).sendKeys("Make your Event Easy");
+				 Thread.sleep(2000);
+				 driver.findElement(next3).click();
+				 Thread.sleep(4000);
+				 
+				 
+				 //section4
+				 js.executeScript("window.scrollBy(0,600)");
+				 driver.findElement(termsAndCon).sendKeys("Must Carry Your Id card");
+				 Thread.sleep(2000);
+				 driver.findElement(eventType).click();
+				 Select oSelect = new Select(driver.findElement(eventType));
+					oSelect.selectByIndex(7);
+					Thread.sleep(2000);
+				 driver.findElement(eventCategory).click();
+				 Select oSelect2 = new Select(driver.findElement(eventCategory));
+					oSelect2.selectByIndex(2);
+					Thread.sleep(2000);
+					driver.findElement(pageName).sendKeys("Stand_stand");
+					driver.findElement(langues).click();
+					//WebElement elementToClick = driver.findElement(checkBox);
+				//	elementToClick.click();				
+				      driver.findElement(checkarabic).click();
+				      driver.findElement(checkenglish).click();
+				      Thread.sleep(2000);
+				      driver.findElement(additionalaNote).sendKeys("Masti Masti");
+				      Thread.sleep(2000);
+				      WebElement elementToClick1 = driver.findElement(publi);
+						elementToClick1.click();
+				 Thread.sleep(2000);
+				 //driver.findElement(saveEventBtn).click();
+				// Thread.sleep(5000);
+				 
+				// driver.findElement(ok).click();
+				// Thread.sleep(3000);		
+				
+			}
+			
+			
+			
 	}
--- src/test/java/wanasa/Emcsuite.java
+++ src/test/java/wanasa/Emcsuite.java
@@ -1,5 +1,7 @@
 package wanasa;
 
+import java.awt.AWTException;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,42 +11,49 @@ import utils.BrowserFunctions;
 
 public class Emcsuite  extends BrowserFunctions{
 	
-	Emc es=new Emc();
-	
+			Emc es=new Emc();
+			
+				
+		 //  @Test(priority=0)
+			  
+			  public void userSignUp() throws InterruptedException
+			  {
+			  
+			  String sucessmsg= es.signUp(); 
+			  Assert.assertEquals(sucessmsg,"Your account has been created successfully");
+			  driver.navigate().to("http://staging.emc.wanasatime.com/"); 
+			  }
 		
-   @Test(priority=0)
-	  
-	  public void userSignUp() throws InterruptedException
-	  {
-	  
-	  String sucessmsg= es.signUp(); 
-	  Assert.assertEquals(sucessmsg,"Your account has been created successfully");
-	  driver.navigate().to("http://staging.emc.wanasatime.com/"); 
-	  }
-
-   @Test(priority=1)
-     
-   public void forgotPswd()
-   {
-	  String forEmail=es.forgotPassword();
-	  Assert.assertEquals(forEmail,"We have sent a mail to you.Please check your mail.");
-	  //driver.navigate().to("http://staging.emc.wanasatime.com/");
-	  driver.findElement(es.loginbtnInForgot).click();
-  }
-   
-	 
- @Test(priority=2)
-
-    public void verifylogin() throws InterruptedException  
-  {	
-	String emptyEmail=es.signInEmptyEmail();
-	Assert.assertEquals(emptyEmail, "Please enter Email");
-	String emptypwd = es.signInEmptyPassword();
-	Assert.assertEquals(emptypwd,"Please enter Password");
-	String invalidinput= es.signInInvalidCredentials();
-	Assert.assertEquals(invalidinput,"EmailId not Registered" );
-	String afterlogin = es.signInValidCredentials();
-	Assert.assertEquals(afterlogin, "http://staging.emc.wanasatime.com/Dashboard.aspx");	
-  }
- 
+		   @Test(priority=1)
+		     
+		   public void forgotPswd()
+		   {
+			  String forEmail=es.forgotPassword();
+			  Assert.assertEquals(forEmail,"We have sent a mail to you.Please check your mail.");
+			  //driver.navigate().to("http://staging.emc.wanasatime.com/");
+			  driver.findElement(es.loginbtnInForgot).click();
+		  }
+		   
+			 
+		 @Test(priority=2)		
+		    public void verifylogin() throws InterruptedException  
+			  {	
+				String emptyEmail=es.signInEmptyEmail();
+				Assert.assertEquals(emptyEmail, "Please enter Email");
+				String emptypwd = es.signInEmptyPassword();
+				Assert.assertEquals(emptypwd,"Please enter Password");
+				String invalidinput= es.signInInvalidCredentials();
+				Assert.assertEquals(invalidinput,"EmailId not Registered" );
+				String afterlogin = es.signInValidCredentials();
+				Assert.assertEquals(afterlogin, "http://staging.emc.wanasatime.com/Dashboard.aspx");	
+			  }
+			 
+		 @Test
+		 public void verifyevent1() throws InterruptedException, AWTException
+		 {
+			 String afterlogin = es.signInValidCredentials();
+			 Assert.assertEquals(afterlogin, "http://staging.emc.wanasatime.com/Dashboard.aspx");
+			 es.evntcreationmultisubcattimeslot();
+		 }
+		 
 }
